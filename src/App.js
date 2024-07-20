d2a2317856385d4e50ae8e1fe1a946e9a00aed09
i-dev Software Group <idev.softwaregroup@gmail.com>
2024-07-20 19:03:36 +0330

    finall editted files here ...

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./Pages/ProfilePage/ProfilePage";
-import Profiles from "./Pages/All/Profiles";
+import Profiles from "../src/Pages/AllProfiles/Profiles";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Profiles />} />
+        {/* <Route path="/" element={<Profiles />} /> */}
+<Route path="/" element={<Profiles />} />
         <Route path="profile/:id" element={<ProfilePage />} />
       </Routes>
     </BrowserRouter>
